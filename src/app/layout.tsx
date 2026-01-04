343925ad9a7e66978fabd5f290ada7d4d29622f7
BatuhanYolcu1 <bthnylc1@gmail.com>
2026-01-04 18:01:46 +0300

    style: change font from Lexend to Poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
       </head>
-      <body className="antialiased min-h-screen bg-[#0b0f17] text-white font-['Lexend']">
+      <body className="antialiased min-h-screen bg-[#0b0f17] text-white font-['Poppins']">
         <Providers>
           <Navbar />
           <main className="pt-24 pb-12">
